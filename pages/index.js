38be602cfa17000ba5fc4b577abf6fb32bca728d
Kazim Akgul <kazimakgul@gmail.com>
2023-08-04 02:37:18 +0300

    last fix

--- pages/index.js
+++ pages/index.js
@@ -19,18 +19,7 @@ export default function NerkoTemplate() {
         <Layout>
 			 <Head>
                 <title>LotShare - Accessible and Flexible Land Investments with NFTs</title>
-				<meta name="description" content="LotShare is a decentralized platform for buying, selling, and trading land NFTs using the LOT token. Invest in prime real estate and earn staking rewards, transaction discounts, revenue sharing, and more." />
-				<meta name="keywords" content="LotShare, NFT, land NFT, LOT token, real estate, staking rewards, transaction discounts, revenue sharing" />
-				<meta name="author" content="LotShare" />
-				
-				<meta property="og:title" content="LotShare - Accessible and Flexible Land Investments with NFTs" />
-				<meta property="og:description" content="LotShare is a decentralized platform for buying, selling, and trading land NFTs using the LOT token. Invest in prime real estate and earn staking rewards, transaction discounts, revenue sharing, and more." />
-				<meta property="og:image" content="https://lotshare.app/images/logo/socialLot.png" />
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://lotshare.app" />
-				<meta property="twitter:title" content="LotShare - Accessible and Flexible Land Investments with NFTs" />
-				<meta property="twitter:description" content="LotShare is a decentralized platform for buying, selling, and trading land NFTs using the LOT token. Invest in prime real estate and earn staking rewards, transaction discounts, revenue sharing, and more." />
-				<meta property="twitter:image" content="https://lotshare.app/images/logo/socialLot.png" />
+
             </Head>			
 			<HeroBannerOne/>
 			<br></br>
